90d0e8d4c432e59f0a669b7da3866717c8327150
Willie Bam <meilinli1013@gmail.com>
2026-07-24 09:16:29 +0800

    dev: return as json

--- backend/types/models/team_member.go
+++ backend/types/models/team_member.go
@@ -5,11 +5,11 @@ import (
 )
 
 type TeamMember struct {
-	ID     uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
-	TeamID uuid.UUID `gorm:"type:uuid;not null;index;uniqueIndex:idx_user_team"`
-	UserID uuid.UUID `gorm:"type:uuid;not null;index;uniqueIndex:idx_user_team"`
-	Role   string    `gorm:"type:varchar(20);not null;default:'member'"`
+	ID     uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey" json:"id"`
+	TeamID uuid.UUID `gorm:"type:uuid;not null;index;uniqueIndex:idx_user_team" json:"team_id"`
+	UserID uuid.UUID `gorm:"type:uuid;not null;index;uniqueIndex:idx_user_team" json:"user_id"`
+	Role   string    `gorm:"type:varchar(20);not null;default:'member'" json:"role"`
 
-	Team Team `gorm:"foreignKey:TeamID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
-	User User `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	Team Team `gorm:"foreignKey:TeamID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE" json:"team"`
+	User User `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE" json:"user"`
 }
--- backend/types/models/teams.go
+++ backend/types/models/teams.go
@@ -7,8 +7,8 @@ import (
 )
 
 type Team struct {
-	ID        uuid.UUID    `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
-	TeamName  string       `gorm:"type:varchar(20);not null;uniqueIndex"`
-	CreatedAt time.Time    `gorm:"type:timestamp(0);default:CURRENT_TIMESTAMP"`
-	Members   []TeamMember `gorm:"foreignKey:TeamID;constraint:OnDelete:CASCADE"`
+	ID        uuid.UUID    `gorm:"type:uuid;default:uuid_generate_v4();primaryKey" json:"id"`
+	TeamName  string       `gorm:"type:varchar(20);not null;uniqueIndex" json:"team_name"`
+	CreatedAt time.Time    `gorm:"type:timestamp(0);default:CURRENT_TIMESTAMP" json:"created_at"`
+	Members   []TeamMember `gorm:"foreignKey:TeamID;constraint:OnDelete:CASCADE" json:"members,omitempty"`
 }
